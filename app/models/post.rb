75c28778aba3d10d4f3a6dff949a5f6b78a03709
Almudena <almudenaprt@gmail.com>
2018-03-07 09:39:45 +0100

    Incluir imágenes en los posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,16 @@
 class Post < ApplicationRecord
     has_many :comments    #esto no se genera automáticamente cuando hemos creado el scaffold. Lo añadimos después
-
     belongs_to :user
 
+    mount_uploader :image, ImageUploader
+
     paginates_per 2
+
+    def short_entry
+        if blog_entry.length > 120
+            blog_entry[0..120] + "..."
+        else
+            blog_entry
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180306095624) do
+ActiveRecord::Schema.define(version: 20180306155651) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20180306095624) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
