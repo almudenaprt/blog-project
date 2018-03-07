0ce4b6577a4a697328a09322c3df3b4e45ea4b32
Almudena <almudenaprt@gmail.com>
2018-03-07 12:33:42 +0100

    Añadir vista para todos los posts de un usuario

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :comments   # Con esto accedemos a todas las rutas del controlador, sin tener que indicarlas aquí una por una
   resources :posts   
+
+  # Ruta para acceder a todos los posts que haya creado un usuario
+  get '/:name' => 'posts#user_posts', as: :user_posts
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'posts#index'
